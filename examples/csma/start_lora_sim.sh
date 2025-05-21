0e66fd1f764147552c91fd59ace35b1bd80e46cb
erkath <eremenko.kath@gmail.com>
2025-05-22 01:38:26 +0300

    start_lora_sim.sh

--- examples/csma/start_lora_sim.sh
+++ examples/csma/start_lora_sim.sh
@@ -1,37 +1,17 @@
 # /bin/bash!
 
-#./ns3 run "scratch/aloha-throughput.cc --CsmaEnabled=false --interferenceMatrix=goursaud --nDevices=1000 --filePostfix=false" >>./3km_400s_lorawan_adr_without_cad-100.log 2>&1 &
-#pid1=$!
-#./ns3 run "scratch/aloha-throughput.cc --CsmaEnabled=true --interferenceMatrix=goursaud --nDevices=1000 --filePostfix=true" >>./3km_400s_lorawan_adr_cad-100.log 2>&1 &
-#pid2=$!
-#wait $pid1 $pid2
+device_counts=(50 100 150 200 250 300 350 400 450)
 
-./ns3 run "scratch/aloha-throughput.cc --CsmaEnabled=false --interferenceMatrix=goursaud --nDevices=20 --filePostfix=false" >>./3km_400s_lorawan_adr_without_cad-2000.log 2>&1
-pid1=$!
-./ns3 run "scratch/aloha-throughput.cc --CsmaEnabled=true --interferenceMatrix=goursaud --nDevices=20 --filePostfix=true" >>./3km_400s_lorawan_adr_cad-2000.log 2>&1
-pid2=$!
+for nDevices in "${device_counts[@]}"
+do
+    # Запуск без CAD
+    ./ns3 run "scratch/aloha-throughput.cc --CsmaEnabled=false --interferenceMatrix=goursaud --nDevices=$nDevices --filePostfix=false" >> "./80m_100s_lorawan_adr_without_cad-${nDevices}.log" 2>&1 &
+    pid1=$!
+    wait $pid1
 
-#
-#
-#./ns3 run "scratch/aloha-throughput.cc --CsmaEnabled=false --interferenceMatrix=goursaud --nDevices=4000 --filePostfix=false" >>./3km_400s_lorawan_adr_without_cad-4000.log 2>&1
-#pid1=$!
-#./ns3 run "scratch/aloha-throughput.cc --CsmaEnabled=true --interferenceMatrix=goursaud --nDevices=4000 --filePostfix=true" >>./3km_400s_lorawan_adr_cad-4000.log 2>&1
-#pid2=$!
-#
-#
-#./ns3 run "scratch/aloha-throughput.cc --CsmaEnabled=false --interferenceMatrix=goursaud --nDevices=10000 --filePostfix=false" >>./3km_400s_lorawan_adr_without_cad-10000.log 2>&1
-#pid1=$!
-#./ns3 run "scratch/aloha-throughput.cc --CsmaEnabled=true --interferenceMatrix=goursaud --nDevices=10000 --filePostfix=true" >>./3km_400s_lorawan_adr_cad-10000.log 2>&1
-#pid2=$!
+    # Запуск с CAD
+    ./ns3 run "scratch/aloha-throughput.cc --CsmaEnabled=true --interferenceMatrix=goursaud --nDevices=$nDevices --filePostfix=true" >> "./80m_100s_lorawan_adr_cad-${nDevices}.log" 2>&1 &
+    pid2=$!
+    wait $pid2
 
-##./ns3 run "scratch/aloha-throughput.cc --CsmaEnabled=false --interferenceMatrix=goursaud --nDevices=750 --filePostfix=false" >>./3km_400s_lorawan_adr_without_cad-750.log 2>&1 &
-##pid1=$!
-##./ns3 run "scratch/aloha-throughput.cc --CsmaEnabled=true --interferenceMatrix=goursaud --nDevices=750 --filePostfix=true" >>./3km_400s_lorawan_adr_cad-750.log 2>&1 &
-##pid2=$!
-##wait $pid1 $pid2
-#
-#./ns3 run "scratch/aloha-throughput.cc --CsmaEnabled=false --interferenceMatrix=goursaud --nDevices=1000 --filePostfix=false" >>./3km_400s_lorawan_adr_without_cad-1000.log 2>&1 &
-#pid1=$!
-#./ns3 run "scratch/aloha-throughput.cc --CsmaEnabled=true --interferenceMatrix=goursaud --nDevices=1000 --filePostfix=true" >>./3km_400s_lorawan_adr_cad-1000.log 2>&1 &
-#pid2=$!
-#wait $pid1 $pid2
+done
